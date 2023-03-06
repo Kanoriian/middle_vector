09b6d165f766a4c6e428280446440e3991376060
Kanorian <112107919+Kanoriian@users.noreply.github.com>
2023-03-06 17:29:22 +0300

    Update middle_vector1.cpp

--- middle_vector1.cpp
+++ middle_vector1.cpp
@@ -10,7 +10,7 @@ vector <char> itc_strtlist(string str){ //1
     return mass;
 }
 
-string itc_join(vector <char> lst, string sep){
+string itc_join(vector <char> lst, string sep){ //2
     string str;
     for(int i = 0; i < str.size(); i++){
         str += mass[i] + sep;
@@ -18,7 +18,7 @@ string itc_join(vector <char> lst, string sep){
     return mass;
 }
 
-string itc_rmstrspc(string str){
+string itc_rmstrspc(string str){ //3
     string stroka;
     for(int i = 0; i < str.size(); i++){
         if(str[i] != ' '){
@@ -26,4 +26,28 @@ string itc_rmstrspc(string str){
         }
     }
     return stroka;
+}
+
+string itc_rmstrchar(string str, string ls){ //4
+    string result = "";
+    for (int i = 0; i < str.size(); i++){
+        int a;
+        for (a = 0; a < ls.size(); a++){
+            if (str[i] == ls[a]){
+                break;
+            }
+        }
+        if (a == ls.size()){
+            result += str[i];
+        }
+    }
+    return result;
+}
+
+long itc_sumlst(const vector <int> &lst){ //5
+    long qwerty = 0;
+    for(long i = 0; i < lst.size(); i++){
+        qwerty += lst[i];
+    }
+    return qwerty;
 }
