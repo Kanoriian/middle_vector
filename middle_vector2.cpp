d59a2dfa9f8b93c32bf9f597f788e64bbb364b30
Kanorian <112107919+Kanoriian@users.noreply.github.com>
2023-03-06 17:52:39 +0300

    Update middle_vector2.cpp

--- middle_vector2.cpp
+++ middle_vector2.cpp
@@ -1,3 +1,35 @@
 #include<middle_list.h>
 
 using namespace std;
+
+long itc_sum_even_lst(const vector <int> &lst){ //6
+    long qwerty = 0;
+    for(long i = 0; i < lst.size(); i+=2){
+        qwerty += lst[i];
+    }
+    return qwerty;
+}
+
+long itc_sum_even_part_lst(const vector <int> &lst){ //7
+    long qwerty = 0;
+    for(long i = 0; i < lst.size(); i++){
+        if(lst[i] % 2 == 0){
+            qwerty += lst[i];
+        }
+    }
+    return qwerty;
+}
+
+
+void itc_odd_even_separator_lst(const vector <int> &lst, vector <int> &lst1, vector <int> &lst2){ // 8
+    long a = 0, b = 0;
+    for(long i = 0; i < lst.size(); i++){
+        if(lst[i] % 2 == 0){
+            lst1[a] = lst[i];
+            a++;
+        }else{
+            lst2[b] = lst[i];
+            b++;
+        }
+    }
+}
