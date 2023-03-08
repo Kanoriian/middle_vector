94b60d5ac60ae12c95651ce2291858ae9b476bf2
Kanorian <112107919+Kanoriian@users.noreply.github.com>
2023-03-08 12:29:17 +0300

    Update middle_vector2.cpp

--- middle_vector2.cpp
+++ middle_vector2.cpp
@@ -33,3 +33,19 @@ void itc_odd_even_separator_lst(const vector <int> &lst, vector <int> &lst1, vec
         }
     }
 }
+
+void itc_pos_neg_separator_lst(const vector <int> &lst, vector <int> &lst1, vector <int> &lst2, vector <int> &lst3){ //9
+    long a = 0, b = 0, c = 0;
+    for(int i = 0; i < lst.size(); i++){
+        if(lst[i] < 0){
+            lst1[a] = lst[i];
+            a++;
+        }if(lst[i] == 0){
+            lst2[b] = lst[i];
+            b++;
+        }else{
+            lst3[c] = lst[i];
+            c++;
+        }
+    }
+}
